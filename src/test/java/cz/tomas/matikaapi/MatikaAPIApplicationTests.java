cbb34338006dec5695ca907732eac952c3389d48
TomasHampl <tomas.hampl@gmail.com>
2024-01-28 13:46:42 +0100

    Initial commit

---
+++ src/test/java/cz/tomas/matikaapi/MatikaAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tomas.matikaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatikaAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
